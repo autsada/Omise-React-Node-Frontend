2a7e6f20c3a43956c05db0fda235ec80953aa1e7
autsada <autsada.t@gmail.com>
2019-03-25 20:47:52 +0700

    Starter V3

--- src/components/checkoutForm/omise-prebuilt-form/CheckoutCreditCard.js
+++ src/components/checkoutForm/omise-prebuilt-form/CheckoutCreditCard.js
@@ -1,14 +1,65 @@
 import React, { Component } from "react";
+import Script from "react-load-script";
 
 import "./Checkout.css";
 
+let OmiseCard;
+
 export class Checkout extends Component {
+  handleLoadScript = () => {
+    OmiseCard = window.OmiseCard;
+    OmiseCard.configure({
+      publicKey: "pkey_test_5ex05ynxfnnc33txbyx",
+      currency: "thb",
+      frameLabel: "Sabai Shop",
+      submitLabel: "PAY NOW",
+      buttonLabel: "Pay with Omise"
+    });
+  };
+
+  creditCardConfigure = () => {
+    OmiseCard.configure({
+      defaultPaymentMethod: "credit_card",
+      otherPaymentMethods: []
+    });
+    OmiseCard.configureButton("#credit-card");
+    OmiseCard.attach();
+  };
+
+  omiseCardHandler = () => {
+    OmiseCard.open({
+      frameDescription: 'Invoice #3847',
+      amount: 10000,
+      onCreateTokenSuccess: (token) => {
+        console.log(token)
+      },
+      onFormClosed: () => {},
+    })
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    this.creditCardConfigure();
+    this.omiseCardHandler()
+  };
+
   render() {
     return (
       <div className="own-form">
-        <button className="btn" type="button">
-          Pay with Credit Card
-        </button>
+        <Script
+          url="https://cdn.omise.co/omise.js"
+          onLoad={this.handleLoadScript}
+        />
+        <form>
+          <button
+            id="credit-card"
+            className="btn"
+            type="button"
+            onClick={this.handleClick}
+          >
+            Pay with Credit Card
+          </button>
+        </form>
       </div>
     );
   }
--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -26,28 +26,9 @@ export class CartCheckoutPage extends Component {
         <CheckoutInternetBanking
           cart={cart}
         />
-        {/* <div className="message">
-          {charge && (
-            <div>
-              <h4>Thank you for your payment with credit card.</h4>
-              <p>
-                Your payment amount is{" "}
-                <span className="amount">{new Intl.NumberFormat().format(charge.amount)} Baht</span>, status:{" "}
-                <span
-                  className={
-                    charge.status === "successful"
-                      ? "success"
-                      : charge.status === "failed"
-                      ? "failed"
-                      : "pending"
-                  }
-                >
-                  {charge.status}
-                </span>
-              </p>
-            </div>
-          )}
-        </div> */}
+        <div className="message">
+
+        </div>
       </div>
     );
   }
